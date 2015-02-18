0fadf08e25109c70d50f8350ad0f8ca590d91335
hugo <hugo@soixantecircuits.fr>
2015-02-18 17:22:46 +0100

    allow all incoming methods

--- app.js
+++ app.js
@@ -64,6 +64,7 @@
 
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
