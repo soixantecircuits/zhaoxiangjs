fb8989a29025ccf296c991f5a591421544a08bc7
hugo <hugo@soixantecircuits.fr>
2015-02-18 12:34:15 +0100

    allow CORS

--- app.js
+++ app.js
@@ -62,6 +62,12 @@
 
   app = express();
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use(express["static"](__dirname + '/public'));
 
   //app.use(express.bodyParser());
