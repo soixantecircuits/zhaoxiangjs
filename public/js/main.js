6d72aab07ced1c8a21e907502b8d6a186ec34f08
Arthur Battut <arthur.battut@gmail.com>
2015-09-10 19:15:12 +0200

    it's the final countdown

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,49 @@
 var Main = (function(my, GPhoto){
-  
+  my.gif = {};
+
   my.initSocket = function(){
     var socket = io.connect('http://localhost:1789');
     socket.on('countdown', function(duration){
-      my.countdown(duration, document.getElementById('counter'), function(){
-        console.log('shot taken');
-        setTimeout(function(){
-          document.getElementById('counter').innerHTML = '';
-        }, 1000);
-      });
+      my.showGifCount(duration);
     });
   };
 
-  my.countdown = function(duration, display, callback) {
-    var timer = duration, seconds;
-    var interval = setInterval(function () {
-      seconds = parseInt(timer % 60, 10);
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-      display.textContent = seconds;
-      if (--timer < 0) {
-        timer = duration;
-      }
-      if(seconds == 0){
-        display.innerHTML = "Souriez !";
-        clearInterval(interval);
-        callback();
-      }
-    }, 1000);
+  my.showGifCount = function(duration){
+    $('#imageWrap').fadeOut('400');
+    $('#counter').fadeIn(400, function() {
+      my.gif.play();
+      setTimeout(function(){
+        $('#imageWrap').fadeIn('400');
+        $('#counter').fadeOut('400', function() {
+          my.gif.pause();
+          my.gif.move_to(0);
+        });
+      }, duration*1000);
+    });
   };
 
   my.initStream = function(){
     GPhoto.startPreview(8088);
   };
 
+  my.initGif = function(){
+    $(document).ready(function() {
+      var images = $('img');
+
+      if (/.*\.gif/.test(images[0].src)) {
+        console.log('salut');
+        my.gif = new SuperGif({ gif: images[0] } );
+        my.gif.load(function(){
+          console.log('Countdown gif loaded');
+        });
+      }
+    });
+  }
+
   my.init = function(){
     my.initSocket();
     my.initStream();
+    my.initGif();
   };
 
   return my;
